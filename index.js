c98721ef2150675e4a2be012345a4c228e35ee21
Ihor Melnyk <igor.melnick85@gmail.com>
2023-10-05 10:00:32 +0300

    finish code

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ function onTaskExecuteSendDismissal(routeStage) {
 }
 
 function sendMail() {
+  //var emailInitiator1 = EdocsApi.getEmployeeDataByEmployeeID(EdocsApi.getAttributeValue("Initiator1").value).email;
   var Date1 = EdocsApi.getAttributeValue("Date1").value;
   var methodData = {
-    recipients: ["igor.melnick85@gmail.com"],
+    recipients: [
+      EdocsApi.getEmployeeDataByEmployeeID(
+        EdocsApi.getAttributeValue("Initiator1").value
+      ).email,
+    ],
     subject: "Запит на звільнення",
     body: `<html><body>
-    <a href="https://teams.microsoft.com/l/message/19:93496aac-ae3a-489c-8ba4-8466eb3a2c93_afe6b16b-6711-49e1-901b-716bf4ddd102@unq.gbl.spaces/1696416556680?context=%7B%22contextType%22%3A%22chat%22%7D">Інструкція по створенню Заяви </a>
+    <a href="https://etechua.sharepoint.com/sites/eDocs/instruction/%D0%86%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D1%96%D1%97%20%D0%97%D0%B0%D1%8F%D0%B2%D0%B0%20%D0%BD%D0%B0%20%D0%B7%D0%B2%D1%96%D0%BB%D1%8C%D0%BD%D0%B5%D0%BD%D0%BD%D1%8F.pdf">Інструкції Заява на звільнення.pdf</a>
     <a href="https://online.e-docs.ua/">Посилання на систему еДокс</a>
     Створіть в еДокс заяву про звільнення та запустіть процес її погодження.<br />Дата звільнення – ${
       Date1
@@ -26,7 +31,7 @@ function sendMail() {
           new Date(Date1).getFullYear()
         : " "
     }. Причина звільнення - ${
-      EdocsApi.getAttributeValue("approvalOfParticipants").value
+      EdocsApi.getAttributeValue("Reason_for_leaving")?.value
     }</body></html>`,
   };
 
